e88498f6303293595889825cf774211273d02a9e
lizhu <t_lizhu@channelsoft.com>
2018-03-18 16:14:27 +0800

    test

---
+++ src/main/java/com/channelsoft/springweb/test/InputStreamTest.java
@@ -0,0 +1,17 @@
+package com.channelsoft.springweb.test;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+/**
+ * @Author: lizhu
+ * @ClassName: InputStreamTest
+ * @Desciption: Date:2018/3/18
+ */
+public class InputStreamTest {
+
+    public static void main(String[] args){
+        InputStream inputStream;
+        FileInputStream fileInputStream;
+    }
+}
