e7e063283a77e0cb7e6604334f455b574673c198
lizhu <t_lizhu@channelsoft.com>
2018-04-07 22:31:58 +0800

    Spring Resource

---
+++ src/main/java/com/channelsoft/springweb/bean/resource/ResourceTest.java
@@ -0,0 +1,14 @@
+package com.channelsoft.springweb.bean.resource;
+
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+/**
+ * @Author: lizhu
+ * @ClassName: ResourceTest
+ * @Desciption: Date:2018/4/7
+ */
+public class ResourceTest {
+    public static void main(String[] args){
+        FileSystemXmlApplicationContext applicationContext;
+    }
+}
